bc384f9e0e3ff9774faff0bb68909e2cb54a6d9d
Miłosz Musielak <milosz.musielak2@gmail.com>
2023-06-21 16:05:55 +0200

    czesc 2 #

--- skrypt.sh
+++ skrypt.sh
@@ -20,3 +20,73 @@ elif [[ "$1" == "--help" ]]; then
 else
   echo "Nieznana opcja. Użyj --help, aby wyświetlić dostępne opcje."
 fi
+
+
+display_help() {
+    echo "Użycie: skrypt.sh [OPCJA]"
+    echo "Opcje:"
+    echo "  -h, --help     Wyświetla pomoc"
+    echo "  -l, --logs     Tworzy pliki logx.txt (x = 1-100) z informacjami"
+    echo "  -d, --date     Wyświetla dzisiejszą datę"
+    echo "      --init     Klonuje repozytorium i ustawia ścieżkę w zmiennej PATH"
+    echo "  -e, --error    Tworzy pliki errorx/errorx.txt (x = 1-100)"
+    echo "                 Jeśli nie podano liczby, domyślnie tworzy 100 plików"
+}
+
+create_logs() {
+    for ((i = 1; i <= 100; i++)); do
+        filename="log${i}.txt"
+        echo "Nazwa pliku: $filename" >"$filename"
+        echo "Utworzone przez skrypt.sh" >>"$filename"
+        date >>"$filename"
+    done
+    echo "Utworzono 100 plików logx.txt"
+}
+
+display_date() {
+    date
+}
+
+initialize_repository() {
+    git clone https://github.com/twoje-repozytorium.git
+    export PATH="$PATH:$(pwd)"
+    echo "Repozytorium zostało sklonowane, ścieżka ustawiona w zmiennej PATH"
+}
+
+create_error_files() {
+    count=$1
+    if [[ -z $count ]]; then
+        count=100
+    fi
+    for ((i = 1; i <= count; i++)); do
+        filename="error${i}/error${i}.txt"
+        mkdir -p "$(dirname "$filename")"
+        echo "Nazwa pliku: $filename" >"$filename"
+        echo "Utworzone przez skrypt.sh" >>"$filename"
+        date >>"$filename"
+    done
+    echo "Utworzono $count plików errorx/errorx.txt"
+}
+
+
+case "$1" in
+-h | --help)
+    display_help
+    ;;
+-l | --logs)
+    create_logs
+    ;;
+-d | --date)
+    display_date
+    ;;
+--init)
+    initialize_repository
+    ;;
+-e | --error)
+    shift
+    create_error_files "$@"
+    ;;
+*)
+    echo "Nieznana opcja. Użyj --help/-h, aby wyświetlić pomoc."
+    ;;
+esac
